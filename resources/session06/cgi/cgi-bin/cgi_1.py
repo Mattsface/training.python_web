ca50c5b833cd83c614441f122d1ebfbd3847a85e
MattsFace <spahmatthew@gmail.com>
2015-05-11 21:41:50 -0700

    Finished the cgi inclass work

--- resources/session06/cgi/cgi-bin/cgi_1.py
+++ resources/session06/cgi/cgi-bin/cgi_1.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import cgi
-
+import cgitb
+cgitb.enable()
+1 / 2
+print "Hello world"
 
 cgi.test()
--- resources/session06/cgi/cgi-bin/cgi_sums.py
+++ resources/session06/cgi/cgi-bin/cgi_sums.py
@@ -4,7 +4,18 @@ import cgitb
 import json
 
 cgitb.enable()
+form = cgi.FieldStorage()
+operands = form.getlist('operand')
+total = 0
+for operand in operands:
+    try:
+        value = int(operand)
+    except ValueError:
+        value = 0
+    total += value
+output = str(total)
 
 print "Content-type: text/plain"
+print "Content-Length: %s" % len(output)
 print
-print "Your job is to make this work"
+print output
